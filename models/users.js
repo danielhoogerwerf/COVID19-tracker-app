9d32818b3f457d9cf97e41ebe3540a3300821dad
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-04-02 20:22:45 +0200

    datamodel & seed file v1

---
+++ models/users.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Users Schema
+const UsersSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    role: {
+      type: String,
+      enum: ["GUEST", "HEALTHWORKER", "ADMIN"],
+      default: "GUEST"
+    },
+    region: {
+      type: String,
+      enum: [
+        "Dienst Gezondheid & Jeugd Zuid-Holland Zuid",
+        "GGD Amsterdam",
+        "GGD Brabant-Zuidoost",
+        "GGD Drenthe",
+        "GGD Fryslân",
+        "GGD Gelderland-Zuid",
+        "GGD Gooi en Vechtstreek",
+        "GGD Groningen",
+        "GGD Haaglanden",
+        "GGD Hart voor Brabant",
+        "GGD Hollands-Midden",
+        "GGD Hollands-Noorden",
+        "GGD IJsselland",
+        "GGD Kennemerland",
+        "GGD Limburg-Noord",
+        "GGD Noord- en Oost-Gelderland",
+        "GGD regio Utrecht",
+        "GGD Rotterdam-Rijnmond",
+        "GGD Twente",
+        "GGD West-Brabant",
+        "GGD Zaanstreek/Waterland",
+        "GGD Zeeland",
+        "GGD Zuid-Limburg",
+        "Veiligheids- en Gezondheidsregio Gelderland-Midden"
+      ],
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Users = mongoose.model('Audit', UsersSchema);
+module.exports = Users
---
+++ seed/seeds.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// Models
+const Users = require("../models/users");
+const BSN = require("../models/bsn");
+const Patients = require("../models/patients");
+
+mongoose
+  .connect("mongodb://localhost/covid-19-tracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}" for seeding data...`))
+  .catch(err => console.log(`Error connecting to the MongoDB: ${err}`));
+
+bcrypt.hash(process.env.ADMIN_PASSWORD, 10).then(hash => {
+  Users.create({
+    username: "admin",
+    password: hash,
+    role: "ADMIN",
+    region: "GGD Amsterdam"
+  }).then(usr => {
+    BSN.create({
+      bsnnumber: 123456789,
+      name: "Daniel Hoogerwerf",
+      birthdate: "1982-11-15",
+      age: 37,
+      gender: "Male"
+    }).then(bsn => {
+      Patients.create({
+        bsn: bsn._id,
+        name: "Daniel Hoogerwerf",
+        birthdate: "1982-11-15",
+        healthcareworker: usr._id,
+        status: "Hospitalized",
+        region: "GGD Amsterdam"
+      });
+    });
+  }).then(console.log("All finished succesfully."));
+});
+
