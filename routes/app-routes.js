8f491705067c9a5f2bf05c7c9b0b0c75ba3da8c2
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-04-12 17:52:50 +0200

    fixing db schema Patients en BSN

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -46,12 +46,11 @@ mobileAppRouter.get("/signup/bsn-internal", checkRoles("ADMIN"), (req, res, next
 
 // INTERNAL USE -- POST route BSN SignUp
 mobileAppRouter.post("/signup/bsn-internal", checkRoles("ADMIN"), (req, res, next) => {
-  const { bsn, name, birthdate, age, gender } = req.body;
+  const { bsn, name, birthdate, gender } = req.body;
   BSN.create({
     bsnnumber: bsn,
     name: name,
     birthdate: birthdate,
-    age: age,
     gender: gender,
   })
     .then(() => {
@@ -112,9 +111,9 @@ mobileAppRouter.get("/signup/patient", ensureLogin.ensureLoggedIn("/app/login"),
 // POST route SignUp page
 mobileAppRouter.post("/signup/patient", ensureLogin.ensureLoggedIn("/app/login"), (req, res, next) => {
   
-  const {name,birthdate,age,region,gender,bsn,status} = req.body
+  const {name,birthdate,region,gender,bsn,status} = req.body
 
-  if (name === '' || birthdate === '' || age === '' || region === '' || gender === '' || bsn === '' || status === ''){
+  if (name === '' || birthdate === '' || region === '' || gender === '' || bsn === '' || status === ''){
     res.render('app/signup/app-signup-patient',{errorMessage: 'Please fill in all the required fields'})
     return
   }
@@ -125,18 +124,17 @@ mobileAppRouter.post("/signup/patient", ensureLogin.ensureLoggedIn("/app/login")
 
 // Step 4 - POST route Confirmation page
 mobileAppRouter.post("/signup/confirmation", ensureLogin.ensureLoggedIn("/app/login"), (req, res, next) => {
-  const { name, birthdate, age, region, gender, bsnnumber, status } = req.body;
+  const { name, birthdate, region, gender, bsnnumber, status } = req.body;
   BSN.create({
     bsnnumber: bsnnumber,
     name: name,
     birthdate: birthdate,
-    age: age,
     gender: gender,
   })
     .then((bsn) => {
       Patients.create({
         bsn: bsn._id,
-        history: {"Status": status, Date: moment.utc().format()},
+        history: {"Status": status, Date: new Date()},
         healthcareworker: req.user.id,
         status: status,
         region: region,
@@ -182,7 +180,7 @@ mobileAppRouter.post("/lookup/patient", ensureLogin.ensureLoggedIn("/app/login")
     .populate({
       path: "bsn",
       match: { $or: [{ birthdate: birthdate }, { name: { $regex: ".*" + name, $options: "i" } }] },
-      select: "name birthdate age gender bsnnumber",
+      select: "name birthdate gender bsnnumber",
     })
     .then((data) => {
       res.render("app/lookup/app-lookup-results-patient", { results: data });
@@ -216,7 +214,7 @@ mobileAppRouter.post("/lookup/patient/:id/edit", ensureLogin.ensureLoggedIn("/ap
   const { status } = req.body;
   Patients.updateOne(
     { bsn: req.params.id },
-    { $set: { status: status, updatedAt: new Date() }, $addToSet: { history: [{"Status": status, "Date": moment.utc().format()}] } }
+    { $set: { status: status, updatedAt: new Date() }, $addToSet: { history: [{"Status": status, "Date": new Date()}] } }
   )
     .then((data) => {
       res.render("app/lookup/app-edit-patient-completed", { id: req.params.id });
