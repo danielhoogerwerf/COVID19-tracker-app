21f8c952a2f79961580359ee5eff66b40e19eb4c
Friso <fhoman@gmail.com>
2020-04-19 18:35:59 +0200

    pagination done

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,21 +1,57 @@
 
-// Confirmed Infections Data
-const apiTotalPatients = () => {
-  axios
-    .get(`/api/patientlist-pagination`)
-    .then((response) => {
-     response
-const patients = response.data.message
+function renderPatientList(patients) {
+    const $patientList = document.getElementById('patient-list')
+    $patientList.innerHTML = '';
+   
+        
 patients.forEach(element => {
-    console.log(element)    
-});
+    element.bsn[0].birthdate = moment().format("DD MMM YYYY");   
+    element.createdAt = moment().format("DD MMM YYYY, HH:mm:ss");
+    let row = `<tr><td>${element.bsn[0].bsnnumber}</td>
+    <td>${element.bsn[0].name}</td><td>${element.bsn[0].gender}</td>
+    <td>${element.bsn[0].birthdate}</td><td>${element.status}</td>
+    <td>${element.region}</td><td>${element.createdAt}</td></tr>`
+            $patientList.insertAdjacentHTML('afterbegin', row);
+    
+        })
+    }
+
+
+
+
+// Confirmed Infections Data
+const apiTotalPatients = (pageNr, size) => {
+    axios.get(`/api/patientlist?pageNo=${pageNr}&size=${size}`)
+    .then(response =>  {
+        console.log(response)
+        renderPatientList(response.data.message);
     })
-    .catch((err) => {
-      console.log("Error while getting the data: ", err);    
-    });
+    .catch(err => console.log(err))
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-apiTotalPatients()
+    let currentPage = 1;
+    let numberPatientsPage = 10   
+    const $previous = document.getElementById('previous')
+    const $next = document.getElementById('next')
+    
+    // Generate default patient list
+apiTotalPatients(currentPage, numberPatientsPage)
 
-});
+// Event listener for pagination event
+const $paginationBox = document.getElementById('pagination-box')
+$paginationBox.addEventListener('click', event => {
+    if (event.target.parentElement.id === 'next') {
+        currentPage++
+        apiTotalPatients(currentPage,numberPatientsPage)
+        $previous.classList.remove("disabled");
+            
+    } else if (event.target.parentElement.id === 'previous') {
+        currentPage--
+        apiTotalPatients(currentPage,numberPatientsPage)
+        if (currentPage === 1) {        
+        $previous.classList.add("disabled");
+        }
+        }
+    })
+})
